15733efe5fce950d19101c3a3d9f8f9d08e0857f
svarnam <svarnam@nvidia.com>
2026-08-21 16:48:05 +0530

    Add CPUsWithDRA feature gate
    Signed-off-by: svarnam <svarnam@nvidia.com>

--- pkg/virt-config/feature-gates.go
+++ pkg/virt-config/feature-gates.go
@@ -159,6 +159,10 @@ func (config *ClusterConfig) HostDevicesWithDRAEnabled() bool {
 	return config.isFeatureGateEnabled(featuregate.HostDevicesWithDRAGate)
 }
 
+func (config *ClusterConfig) CPUsWithDRAGateEnabled() bool {
+	return config.isFeatureGateEnabled(featuregate.CPUsWithDRAGate)
+}
+
 func (config *ClusterConfig) ConfigurableHypervisorEnabled() bool {
 	return config.isFeatureGateEnabled(featuregate.ConfigurableHypervisor)
 }
